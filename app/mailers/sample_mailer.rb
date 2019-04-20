26be927fca981bff67ba3bc65acd347bdd9cfec9
modeverv <modeverv@gmail.com>
2019-04-20 21:58:01 +0900

    attachment

--- app/mailers/sample_mailer.rb
+++ app/mailers/sample_mailer.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 class SampleMailer < ApplicationMailer
   def send_when_create
+    #p Dir.pwd
+    file = File.read('app/mailers/sample_mailer.rb')
+    #p file
+    attachments['test.rb'] = file
     mail to:      "modeverv@gmail.com",
          subject: Time.now.to_s
   end
